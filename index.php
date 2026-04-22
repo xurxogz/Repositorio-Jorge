f85a811ba144fe45a15e87133d870f131551d166
Jorge <jorge@gmail.com>
2026-04-22 19:48:12 +0200

    inicio

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+function obtenerUsuarios($conexion) {
+    // codigo
+    $conexion = [];
+    return $conexion;
+}
+?>
+
+<?php 
+
+class Usuario {
+    public function crear($nombre) {
+        // codigo
+    }
+}
+
+
+
+?>
